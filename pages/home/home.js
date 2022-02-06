f015e71a6d2319affc01c175b657660cf3718ecb
btbrad <24836323+btbrad@users.noreply.github.com>
2022-02-06 15:29:07 +0800

    分类筛选

--- pages/home/home.js
+++ pages/home/home.js
@@ -6,7 +6,21 @@ Page({
    */
   data: {
     tabs: ['全部服务', '在提供', '正在找'],
-    currentTabIndex: 0
+    currentTabIndex: 0,
+    categoryList: [
+      {
+        id: 1,
+        name: '保洁'
+      },
+      {
+        id: 2,
+        name: '汽修'
+      },
+      {
+        id: 3,
+        name: '疏通'
+      }
+    ]
   },
 
   /**
@@ -71,5 +85,9 @@ Page({
     this.setData({
       currentTabIndex: index
     })
+  },
+  handleCategoryChange: function (event) {
+    console.log(event)
+    const id = event.currentTarget.dataset.id
   }
 })
