d0ee60431707a8aa99fea1a54fdb511786d17926
btbrad <biantaobrad@gmail.com>
2022-02-09 16:05:08 +0800

    http async/await

--- model/service.js
+++ model/service.js
@@ -1,11 +1,9 @@
 import Http from "../utils/http"
 
 class Service {
-  getServiceList(page, count, category_id = null) {
+  async getServiceList(page, count, category_id = null) {
     console.log('获取服务列表')
-    const res = Http.request({ url: '/service/list', data: { page, count } })
-    console.log(res)
-    return res
+    return Http.request({ url: '/service/list', data: { page, count } })
   }
 }
 
--- utils/http.js
+++ utils/http.js
@@ -3,26 +3,24 @@ import exceptionMessage from '../config/exception-message'
 import wxToPromise from './wx'
 
 class Http{
-  static request({ url, data, method = 'GET' }) {
-    const res = wxToPromise('request', {
+  static async request({ url, data, method = 'GET' }) {
+    const res = await wxToPromise('request', {
       url: `${APIConfig.baseUrl}${url}`,
       data,
       method,
     })
-    return res.then((res) => {
-      console.log(res)
-      // 全局统一的异常处理
-      // 请求成功
-      if (res.statusCode < 400) {
-        return res.data.data
-      }
-      // 请求失败
-      if (res.statusCode === 401) {
-        // token相关操作
-        return
-      }
-      Http._showError(res.data.error_code, res.data.message)
-    })
+    console.log(res)
+    // 全局统一的异常处理
+    // 请求成功
+    if (res.statusCode < 400) {
+      return res.data.data
+    }
+    // 请求失败
+    if (res.statusCode === 401) {
+      // token相关操作
+      return
+    }
+    Http._showError(res.data.error_code, res.data.message)
 
     // wx.request({
     //   url: `${APIConfig.baseUrl}${url}`,
